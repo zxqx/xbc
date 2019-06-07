d7f0208096b84c5d5b88fb07367f4053bceaedb8
Zak Angelle <zaknotzak@gmail.com>
2019-06-07 01:22:48 -0500

    Create POC blockchain instance

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import Blockchain from './Blockchain';
+import { MINE_RATE } from './Block';
+
+const NUMBER_OF_BLOCKS = 10;
 
 const bc = new Blockchain();
 
-for (let i = 0; i < 10; i++) {
-  bc.addBlock([{ transaction: i }]);
+const start = Date.now();
+
+for (let i = 0; i < NUMBER_OF_BLOCKS; i++) {
+  bc.addBlock([{ transaction: i + 1 }]);
 }
+
+const end = Date.now();
+
+console.log(`Actual mine rate: ${((end - start) / 1000) / NUMBER_OF_BLOCKS}s`);
+console.log(`Ideal mine rate: ${MINE_RATE / 1000}s`);
