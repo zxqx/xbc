374560e2c0b791d33d575067f9ed8b3a4968a778
Zak Angelle <zaknotzak@gmail.com>
2019-06-09 16:47:40 -0500

    Update POC app

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,31 @@
+import Miner from './Miner';
 import Blockchain from './Blockchain';
-import TransactionPool from './TransactionPool';
 import Wallet from './Wallet';
 import { MINE_RATE } from './config';
 
 const NUMBER_OF_BLOCKS = 50;
 
-const bc1 = new Blockchain();
-const bc2 = new Blockchain();
-
-const pool = new TransactionPool();
+const miner = new Miner();
+const peerBlockchain = new Blockchain();
 const wallet = new Wallet();
-wallet.balance = 999999;
+wallet.balance = 99999;
 
 const start = Date.now();
 
 for (let i = 0; i < NUMBER_OF_BLOCKS; i++) {
-  const blockData = [wallet.createTransaction(pool, `sjd29ejf9jf92-${i}`, 50)];
-
-  bc1.addBlock(blockData);
+  wallet.createTransaction(miner.transactionPool, `ksjuwn92jfksgkgj-${i}`, 400);
+  miner.mine();
 }
 
 const end = Date.now();
 
-console.log(`Generated blockchain is ${bc2.incomingChainIsValid(bc1.chain) ? 'valid' : 'invalid'}`);
+// eslint-disable-next-line
+console.log(
+  `Generated blockchain is ${peerBlockchain.incomingChainIsValid(miner.blockchain.chain) ? 'valid' : 'invalid'}`,
+);
+
+// eslint-disable-next-line
 console.log(`Actual mine rate: ${((end - start) / 1000) / NUMBER_OF_BLOCKS}s`);
+
+// eslint-disable-next-line
 console.log(`Ideal mine rate: ${MINE_RATE / 1000}s`);
