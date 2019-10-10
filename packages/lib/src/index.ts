425884cc1006ec09142b5d3dfbc818040c1a09c0
Zak Angelle <zaknotzak@gmail.com>
2019-10-09 23:33:59 -0500

    Rename client to lib

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import Miner from './Miner';
 import Blockchain from './Blockchain';
 import Wallet from './Wallet';
@@ -19,22 +20,19 @@ for (let i = 0; i < NUMBER_OF_BLOCKS; i++) {
 
 const end = Date.now();
 
-// eslint-disable-next-line
 console.log(
-  // eslint-disable-next-line
-  `Generated blockchain is ${peerBlockchain.incomingChainIsValid(miner.blockchain.chain) ? 'valid' : 'invalid'}`,
+  `Generated blockchain is ${peerBlockchain.incomingChainIsValid(miner.blockchain.chain)
+    ? 'valid'
+    : 'invalid'}
+  `,
 );
 
-// eslint-disable-next-line
 console.log(`Difficulty adjustments: ${miner.blockchain.chain.map(block => block.difficulty).join('->')}`);
 
-// eslint-disable-next-line
 console.log(`Average computations per block mine: ${miner.blockchain.chain
   .map(block => block.nonce)
   .reduce((total, nonce) => total + nonce, 0) / NUMBER_OF_BLOCKS}`);
 
-// eslint-disable-next-line
 console.log(`Actual mine rate: ${((end - start) / 1000) / NUMBER_OF_BLOCKS}s`);
 
-// eslint-disable-next-line
 console.log(`Ideal mine rate: ${MINE_RATE / 1000}s`);
