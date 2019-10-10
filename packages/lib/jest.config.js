c4eedd0e709b314854890836c7e380a8b0599407
Zak Angelle <zaknotzak@gmail.com>
2019-10-09 23:56:59 -0500

    Add CI config file

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['core-js', '<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['core-js', './jest.setup.js'],
 };
